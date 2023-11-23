c2c76f05ec72038fdeb8696e2d4763404c14136d
Yuliya <yuliya-sycheva19@yandex.ru>
2023-11-23 08:06:30 +0300

    Init commit

--- app/src/main/java/com/itproger/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/itproger/playlistmaker/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.itproger.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonLibrary = findViewById<Button>(R.id.library)
+        val buttonSettings = findViewById<Button>(R.id.settings)
+
+        buttonSearch.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        buttonLibrary.setOnClickListener {
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+
+        buttonSettings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
     }
 }
